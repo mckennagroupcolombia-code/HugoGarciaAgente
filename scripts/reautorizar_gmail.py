4d7a26a3632783f2b9ae6763dc88599f0584b97c
McKenna Group Colombia <mckennagroupcolombia-code@github.com>
2026-07-16 02:00:07 -0500

    Auto-commit: backup nocturno 2026-07-16 02:00

--- scripts/reautorizar_gmail.py
+++ scripts/reautorizar_gmail.py
@@ -25,6 +25,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/gmail.readonly",
     "https://www.googleapis.com/auth/gmail.modify",
     "https://www.googleapis.com/auth/gmail.labels",
+    "https://www.googleapis.com/auth/gmail.settings.basic",
 ]
 
 def main():
